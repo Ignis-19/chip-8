a32306fe610f483df2caa9c34c9fc515fafb6ece
Ircadius <ignis19@pm.me>
2023-08-18 23:13:15 +0700

    implement Mem methods

--- src/mem.rs
+++ src/mem.rs
@@ -15,4 +15,33 @@ impl Mem {
     pub fn load_font(&mut self) {
         self.memory[0..0x50].copy_from_slice(&FONT_SPRITE);
     }
+
+    pub fn load_program(&mut self, program: &[u8]) {
+        let (_reserved, memory) = self.memory.split_at_mut(0x200);
+        memory[0..program.len()].copy_from_slice(program);
+    }
+
+    pub fn read(&self, address: u16) -> u8 {
+        self.memory[address as usize]
+    }
+
+    pub fn write(&mut self, address: u16, value: u8) {
+        self.memory[address as usize] = value;
+    }
+
+    pub fn read_u16(&self, address: u16) -> u16 {
+        let mut word = [0u8; 2];
+        let addr = address as usize;
+
+        word.copy_from_slice(&self.memory[addr..addr + 2]);
+
+        u16::from_be_bytes(word)
+    }
+
+    pub fn write_u16(&mut self, address: u16, value: u16) {
+        let word = value.to_be_bytes();
+        let addr = address as usize;
+
+        self.memory[addr..addr + 2].copy_from_slice(&word);
+    }
 }
